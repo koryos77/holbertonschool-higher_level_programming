d621c533034c9f16a7593c22590801a9aa46788e
koryos77 <k.rouget21@gmail.com>
2025-01-15 11:31:15 +0100

    Add args which print the arguments and the number of arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    args_count = len(sys.argv) - 1
+
+    if args_count == 0:
+        print("0 arguments.")
+    else:
+        if args_count == 1:
+            print("1 argument:")
+        else:
+            print(f"{args_count} arguments:")
+
+    for i in range(1, args_count + 1):
+        print(f"{i}: {sys.argv[i]}")
