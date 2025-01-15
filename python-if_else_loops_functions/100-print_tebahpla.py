f354286000d7dbc6b9118dcce3d6e4e08917a930
koryos77 <k.rouget21@gmail.com>
2025-01-15 08:08:28 +0100

    fixed the way alphabet was printed

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(25, -1, -1):
-    print(chr(122 - i) if i % 2 == 0 else chr(90 - i), end="")
+for i in range(122, 96, -1):
+    if (122 - i) % 2 == 0:
+        print(chr(i), end="")
+    else:
+        print(chr(i - 32), end="")
