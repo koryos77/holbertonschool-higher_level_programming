c5adc342029906e9f2cc68b10ee0b0adf438ef5c
koryos77 <k.rouget21@gmail.com>
2025-01-15 08:31:12 +0100

    trying to beat the checker for advanced task 100

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if (122 - i) % 2 == 0:
-        print(chr(i), end="")
-    else:
-        print(chr(i - 32), end="")
+for i in range(26):
+    print("{:c}".format(122 - i - (i % 2 * 32)), end="")
