7b6b1f8ae1217643b7c833918a2e9788fcdb599a
koryos77 <k.rouget21@gmail.com>
2025-01-16 12:49:26 +0100

    pycodestyle

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -9,16 +9,16 @@ if __name__ == "__main__":
     if args_count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
-    a = int (sys.argv[1])
+
+    a = int(sys.argv[1])
     operator = sys.argv[2]
-    b = int (sys.argv[3])
+    b = int(sys.argv[3])
 
     operators = {
-        '+' : add,
-        '-' : sub,
-        '*' : mul,
-        '/' : div,
+        '+': add,
+        '-': sub,
+        '*': mul,
+        '/': div,
     }
 
     if operator not in operators:
