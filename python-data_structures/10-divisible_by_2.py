fb68b46043526e2c214cdb793836ce3253c43e6f
koryos77 <k.rouget21@gmail.com>
2025-01-21 10:10:42 +0100

    fixed some pycodestyle

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = []
-    for element in my_list:
-        if element %  2 ==  0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
-    return new_list
+    return [num % 2 == 0 for num in my_list]
