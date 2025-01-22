1fb9d60bae920b2ce0207c5fb5a34459fed9400f
koryos77 <k.rouget21@gmail.com>
2025-01-22 12:05:01 +0100

    same than the commit before

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
-        new_dict = a_dictionary.copy()
-        del new_dict[key]
-        return new_dict
+        del a_dictionary[key]
     return a_dictionary
