022d745530de3dfe98bf2b449de1e8be602f58fb
koryos77 <k.rouget21@gmail.com>
2025-01-22 11:23:57 +0100

    adding all the tasks for project 2

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman_dict = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+    result = 0
+    prev = 0
+    for i in reversed(roman_string):
+        current = roman_dict[i]
+        if current >= prev:
+            result += current
+        else:
+            result -= current
+        prev = current
+    return result
