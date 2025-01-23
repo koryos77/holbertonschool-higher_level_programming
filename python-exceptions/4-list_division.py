5edbe614dec91e7ab573a9cfb3d5dec0858fac9e
koryos77 <k.rouget21@gmail.com>
2025-01-23 11:02:15 +0100

    adding finally

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -9,8 +9,10 @@ def list_division(my_list_1, my_list_2, list_length):
                 result[i] = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
-        except TypeError:
-            print("wrong type")
         except IndexError:
             print("out of range")
-    return result
+        except TypeError:
+            print("wrong type")
+        finally:
+            pass
+        return result
