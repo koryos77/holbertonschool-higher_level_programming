c1102b2b4422b51a21900d8f513d8282d9a0edeb
koryos77 <k.rouget21@gmail.com>
2025-01-26 10:41:41 +0100

    fixed pycdestyle and add new tests

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -29,16 +29,16 @@ def matrix_divided(matrix, div):
     if not matrix or not isinstance(matrix, list):
         raise TypeError("matrix must be a matrix (list of lists) of "
                         "integers/floats")
-    
+
     if not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of "
                         "integers/floats")
-    
+
     if not all(isinstance(elements, (int, float))
                for row in matrix for elements in row):
         raise TypeError("matrix must be a matrix (list of lists) of "
                         "integers/floats")
-    
+
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
