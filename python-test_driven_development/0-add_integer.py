84a0f8a4f634203e05c703a2d12500024e1bb3d0
koryos77 <k.rouget21@gmail.com>
2025-01-26 10:00:19 +0100

    update the py file to fit tests

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -26,6 +26,10 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+    if a == float('inf') or a == float('-inf'):
+        raise OverflowError("float overflow")
+    if b == float('inf') or b == float('-inf'):
+        raise OverflowError("float overflow")
     a = int(a)
     b = int(b)
 
