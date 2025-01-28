c4dba23bd14740cfe59d39f71d790a9e0116ccae
koryos77 <k.rouget21@gmail.com>
2025-01-28 18:00:10 +0100

    modified the for loop  in my_print

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -100,4 +100,4 @@ class Square:
             for _ in range(self.__position[1]):
                 print()
             for i in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+                print("{}{}".format(" " * self.__position[0], "#" * self.__size))
