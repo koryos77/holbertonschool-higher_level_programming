62e150ca90290b3fbac3087438428e2d9c7c4b51
koryos77 <k.rouget21@gmail.com>
2025-01-30 10:33:28 +0100

    corrected the pycodestyle

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -23,6 +23,7 @@ class Node:
             raise TypeError("next_node must be a Node object")
         self._next_node = value
 
+
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
@@ -37,13 +38,14 @@ class SinglyLinkedList:
 
     def sorted_insert(self, value):
         new_node = Node(value)
-        
+
         if self.head is None or self.head.data >= value:
             new_node.next_node = self.head
             self.head = new_node
         else:
             current = self.head
-            while current.next_node is not None and current.next_node.data < value:
+            while (current.next_node is not None and
+                   current.next_node.data < value):
                 current = current.next_node
             new_node.next_node = current.next_node
             current.next_node = new_node
