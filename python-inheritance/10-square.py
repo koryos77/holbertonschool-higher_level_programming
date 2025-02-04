6586568dba445e23069a12904c544f4855632a6b
koryos77 <k.rouget21@gmail.com>
2025-02-04 12:58:48 +0100

    add 10 and 11 tasks

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Class Square that inherits from Rectangle
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(BaseGeometry):
+    """
+    class Square that inherits from Rectangle
+    """
+    def __init__(self, size):
+        """
+        Instantiation with size
+        
+        arguments:
+            _size(int): Positive integer of the size of the square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Public instance method that returns the area of the square.
+        """
+        return self.__size * self.__size
