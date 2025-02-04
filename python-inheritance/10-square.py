5eec6f21c83bad57489b164706d808c9f9854970
koryos77 <k.rouget21@gmail.com>
2025-02-04 13:18:59 +0100

    update: wrong module imported

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,10 +2,10 @@
 """
 Class Square that inherits from Rectangle
 """
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square(BaseGeometry):
+class Square(Rectangle):
     """
     class Square that inherits from Rectangle
     """
@@ -18,6 +18,7 @@ class Square(BaseGeometry):
         """
         self.integer_validator("size", size)
         self.__size = size
+        Rectangle.__init__(self, size, size)
 
     def area(self):
         """
