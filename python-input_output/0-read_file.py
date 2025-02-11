db22a70ddf1b982ec4be9c772e624d060a2fe711
koryos77 <k.rouget21@gmail.com>
2025-02-11 10:30:01 +0100

    test with r

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,6 +8,6 @@ def read_file(filename=""):
     argument:
         filename: the file we want to read and print.
     """
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
