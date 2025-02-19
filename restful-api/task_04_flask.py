706b9a2dc02d2c0312bdc1ebe79cfe651f58eaa5
koryos77 <k.rouget21@gmail.com>
2025-02-19 13:51:09 +0100

    add docstring

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-
-
 from flask import Flask
 from flask import jsonify
 from flask import request
@@ -14,21 +11,49 @@ users = {}
 
 @app.route('/')
 def home():
+    """
+    Home endpoint that returns a welcome message.
+
+    Returns:
+        str: A welcome message for the Flask API.
+    """
     return "Welcome to the Flask API!"
 
 
 @app.route('/data')
 def get_usernames():
+    """
+    Endpoint to retrieve all usernames.
+
+    Returns:
+        flask.Response: A JSON response containing a list of all usernames.
+    """
     return jsonify(list(users.keys()))
 
 
 @app.route('/status')
 def get_status():
+    """
+    Endpoint to check the API status.
+
+    Returns:
+        str: 'OK' if the API is running.
+    """
     return "OK"
 
 
 @app.route('/users/<username>')
 def get_user(username):
+    """
+    Endpoint to retrieve a specific user's information.
+
+    Args:
+        username (str): The username of the user to retrieve.
+
+    Returns:
+        flask.Response: A JSON response containing the user's information
+        if found, or an error message if the user is not found.
+    """
     user = users.get(username)
     if user:
         return jsonify(user)
@@ -38,6 +63,15 @@ def get_user(username):
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
+    """
+    Endpoint to add a new user.
+
+    Expects a JSON payload with user information, including a 'username' field.
+
+    Returns:
+        flask.Response: A JSON response confirming the addition of the user,
+        or an error message if the username is missing.
+    """
     data = request.get_json()
     username = data.get("username")
     if not username:
