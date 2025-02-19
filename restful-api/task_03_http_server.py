2f630b7bcc26c96656f4675cbbce1f1f0188878e
koryos77 <k.rouget21@gmail.com>
2025-02-19 10:59:54 +0100

    add json data

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,9 +1,3 @@
-#!/usr/bin/python3
-"""
-Main module to create a simple HTTP Server.
-"""
-
-
 import http.server
 import json
 
@@ -25,27 +19,36 @@ class SimpleAPI(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == '/data':
-            data = {"name": "John", "age": 30, "city": "New-York"}
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            self.wfile.write(json.dumps(data).encode('utf-8'))
+            data_info = {
+                "name": "John",
+                "age": 30,
+                "city": "New-York"
+                }
+            self.wfile.write(json.dumps(data_info).encode('utf-8'))
         elif self.path == '/status':
             self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
+            self.send_header('Content-type', 'application/json')
             self.end_headers()
+            data_info = {
+                "status": "OK"
+            }
             self.wfile.write(b"OK")
         elif self.path == '/info':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            data_info = {"version": "1.0", "description":
-                         "A simple API built with http.server"}
-            self.wfile.write(json.dumps(data_info).encode())
+            data_info = {
+                "version": "1.0", "description":
+                "A simple API built with http.server"
+                }
+            self.wfile.write(json.dumps(data_info).encode('utf-8'))
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
-            self.end_headers
+            self.end_headers()
             self.wfile.write(b"Endpoint not found")
 
 
