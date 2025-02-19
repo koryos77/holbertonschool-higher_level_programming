8ea127caa131c03d9a3033a2565855baea22c3e8
koryos77 <k.rouget21@gmail.com>
2025-02-19 11:26:09 +0100

    reorganized add_user

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -42,18 +42,11 @@ def get_user(username):
 @app.route('/add_user', methods=['POST'])
 def add_user():
     data = request.get_json()
-    if 'username' not in data:
+    username = data.get("username")
+    if not username:
         return jsonify({"error": "Username is required"}), 400
-
-    users[data['username']] = {
-        "name": data['name'],
-        "age": data['age'],
-        "city": data['city']
-    }
-    return jsonify({
-        "message": "User added",
-        "user": users[data['username']]
-    }), 201
+    users[username] = data
+    return jsonify({"message": "User added", "user": data}), 201
 
 
 if __name__ == "__main__":
