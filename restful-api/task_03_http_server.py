ed91373d32ea329cdf8ec0d79e40a68bd2f50a9c
koryos77 <k.rouget21@gmail.com>
2025-02-19 10:45:04 +0100

    update of task 03, change order of some functions and modified the mdoule

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -4,11 +4,11 @@ Main module to create a simple HTTP Server.
 """
 
 
-from http.server import HTTPServer, BaseHTTPRequestHandler
+import http.server
 import json
 
 
-class SimpleAPI(BaseHTTPRequestHandler):
+class SimpleAPI(http.server.BaseHTTPRequestHandler):
     """
     Class to manage HTTP requests.
 
@@ -25,11 +25,11 @@ class SimpleAPI(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == '/data':
+            data = {"name": "John", "age": 30, "city": "New-York"}
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            data = {"name": "John", "age": 30, "city": "New-York"}
-            self.wfile.write(json.dumps(data).encode())
+            self.wfile.write(json.dumps(data).encode('utf-8'))
         elif self.path == '/status':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
@@ -39,10 +39,15 @@ class SimpleAPI(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            data_info = {"version": "1.0", "description": "A simple API built with http.server"}
+            data_info = {"version": "1.0", "description":
+                         "A simple API built with http.server"}
             self.wfile.write(json.dumps(data_info).encode())
         else:
-            self.send_error(404, "Endpoint not found")
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers
+            self.wfile.write(b"Endpoint not found")
+
 
 def run_server(port=8000):
     """
@@ -52,9 +57,10 @@ def run_server(port=8000):
         port: default port: 8000
     """
     server_address = ('', port)
-    httpd = HTTPServer(server_address, SimpleAPI)
+    httpd = http.server.HTTPServer(server_address, SimpleAPI)
     print(f"Server running on port {port}")
     httpd.serve_forever()
+
 
 if __name__ == '__main__':
     run_server()
