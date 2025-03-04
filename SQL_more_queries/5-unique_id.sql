b1a81d702d6d344206ad5ae237d303ae70692b6d
koryos77 <k.rouget21@gmail.com>
2025-03-04 11:59:49 +0100

    init commit with the 5th first tasks

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- Creates the table unique_id
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT(1) UNIQUE,
+    name VARCHAR(256)
+);
---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,1 @@
+-- Creates the database hbtn_0d_usa and the table states
