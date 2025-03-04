a1617d080f76d30cc6320094ae6b9e09ff4ec0d8
koryos77 <k.rouget21@gmail.com>
2025-03-04 17:14:38 +0100

    mistyping and table spelling error

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,4 +3,4 @@ SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_show
 FROM tv_show_genres
 LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
-ORDER BY number number_of_shows, tv_genres.id;
+ORDER BY number_of_shows, tv_genres.id;
--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title, genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-LEFT JOIN genres ON tv_show_genres.genre_id = genres.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = genres.id
 ORDER BY tv_shows.title, genres.name;
