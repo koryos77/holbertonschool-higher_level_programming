4778a6e7a226a55a379979d95d5fc53deb554abc
koryos77 <k.rouget21@gmail.com>
2025-03-04 10:18:12 +0100

    put everything in one line for task 11 and add forgotten comment for task 14

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,5 +1,2 @@
 -- Lists all records with a score >= 10 in second_table
-SELECT score, name
-FROM second_table
-WHERE >= 10
-ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE >= 10 ORDER BY score DESC;
