9d36e15dd4d342dc44a9e496dcc3071fb3e4f5f4
koryos77 <k.rouget21@gmail.com>
2025-03-04 17:34:41 +0100

    try something new

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,7 @@
 -- Lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
-FROM tv_show_genres
-LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows, tv_genres.id ASC;
+SELECT genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM genres
+LEFT JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+GROUP BY genres.name
+WHERE COUNT(tv_show_genres.tv_show_id) > 0
+ORDER BY number_of_shows DESC;
--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Lists all shows and all genres linked to that show, from hbtn_0d_tvshows
-SELECT tv_shows.title, tv_genres.name
+SELECT tv_shows.title, genres.name
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY tv_shows.titlen tv_genres.name;
+LEFT JOIN tv_show_genres ON tv_show.id = tv_show_genres.tv_show_id
+LEFT JOIN genres ON tv_show_genres.genre_id = genres.id
+ORDER BY tv_shows.title, genres.name;
