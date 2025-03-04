d0cedd197ea8056546f8e4bb8fdab104a1ff2957
koryos77 <k.rouget21@gmail.com>
2025-03-04 17:10:00 +0100

    remove  ASC

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -4,4 +4,4 @@ FROM tv_shows
 JOIN tv_show_genres ON tv_show.id = tv_show_genres.tv_show_id
 JOIN genres ON tv_show_genres.genre_id = genres.id
 WHERE genres.name = 'Comedy'
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_shows.title;
--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.title, genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 LEFT JOIN genres ON tv_show_genres.genre_id = genres.id
-ORDER BY tv_shows.title ASC, genres.name ASC;
+ORDER BY tv_shows.title, genres.name;
