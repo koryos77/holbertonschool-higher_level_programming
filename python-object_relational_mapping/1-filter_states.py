dc4bd4993342167ff396ec0e9a5f5b0c1f52e86b
koryos77 <k.rouget21@gmail.com>
2025-03-05 15:37:16 +0100

    maybe pycodestyle

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that lists all states with name starting with N from hbtn_0e_0_usa
+Script that lists all states with name
+starting with N from hbtn_0e_0_usa
 """
 import sys
 import MySQLdb
