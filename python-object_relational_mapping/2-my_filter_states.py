b5655dd34ac3b35da96cdc45915f0f7e1f1213e5
koryos77 <k.rouget21@gmail.com>
2025-03-07 14:32:06 +0100

    back to last commit

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,48 +7,45 @@ import sys
 import MySQLdb
 
 
-# Ensures the script runs only if executed directly
 if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>"
-              .format(sys.argv[0]))
+    # Verification of all required arguments
+    if len(sys.argv) != 5:
+        print("Usage: {} <mysql username> <mysql password> <database name>\
+              <state name>".format(sys.argv[0]))
         sys.exit(1)
 
-    # Retrieve command-line arguments.
+    # Command line args
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_name = sys.argv[4]  # The state name to search for
+    state_name = sys.argv[4]
 
-    # Establish a connection to the MySQL database
+    # Connection to MySQL Server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
         passwd=password,
-        db=database,
+        db=database
     )
 
-    # Create a cursor object to execute SQL queries
+    # Creating Cursor
     cursor = db.cursor()
 
-    # Execute the SQL query
+    # Execute the SQL query with format
     query = """
-    SELECT * FROM states WHERE name LIKE BINARY'{}' ORDER BY states.id ASC
+    SELECT * FROM states WHERE name = '{}' ORDER BY id ASC
     """
     query = query.format(state_name)
     cursor.execute(query)
 
-    # Fetch all results from the executed query
-    results = cursor.fetchall()
+    # Fetch all the rows
+    states = cursor.fetchall()
 
-    # Print each row in the result set
-    for row in results:
-        print(row)
+    # Display the results
+    for state in states:
+        print(state)
 
-    # Close the cursor after execution
+    # Close cursor and database connection
     cursor.close()
-
-    # Close the database connection
     db.close()
