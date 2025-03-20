7cb2aa365f150c78c36a812e9dc7afadd1fbbd62
koryos77 <k.rouget21@gmail.com>
2025-03-20 11:37:20 +0100

    test the code return

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import json
 
 app = Flask(__name__)
@@ -17,9 +17,14 @@ def contact():
 
 @app.route('/items')
 def items():
-    with open('items.json', 'r') as file:
-        data = json.load(file)
-    return render_template('items.html', items=data['items']), 200
+    try:
+        with open('items.json', 'r') as file:
+            data = json.load(file)
+        return render_template('items.html', items=data['items']), 200
+    except FileNotFoundError:
+        return jsonify({"error": "File not found"}), 404
+    except json.JSONDecodeError:
+        return jsonify({"error": "Error JSON Format"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
